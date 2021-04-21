85984390af6edbfc42c119098b8e8941b0cbdd4a
Kasper Sogaard <kasper@sogaard.us>
2021-04-20 21:09:38 -0700

    Gradle args supported

--- bin/Gradle.bat
+++ bin/Gradle.bat
@@ -4,5 +4,10 @@ echo Trying to find GradleBuildSystem
 for /f "delims=" %%A in ('maui path [GradleBuildSystem]pkg.dir') do set "GRADLE_PATH=%%A"
 echo Using gradle at %GRADLE_PATH%
 
-echo Executing %GRADLE_PATH%/archipelagoGradleW build
-%GRADLE_PATH%/archipelagoGradleW build
+
+if "%~1"==""
+  echo Executing %GRADLE_PATH%/archipelagoGradleW build
+  %GRADLE_PATH%/archipelagoGradleW build
+else 
+  echo Executing %GRADLE_PATH%/archipelagoGradleW %*
+  %GRADLE_PATH%/archipelagoGradleW %*
