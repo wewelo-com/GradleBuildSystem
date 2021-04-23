50ade73573bac438c23525763e6372a6bebb6d74
Kasper Sogaard <kasper@sogaard.us>
2021-04-23 16:59:36 -0700

    Revert "Fixed windows build"
    This reverts commit 7d8c7aee695872730872c4de9e9971ed9dc86b92.

--- bin/Gradle.bat
+++ bin/Gradle.bat
@@ -5,10 +5,9 @@ for /f "delims=" %%A in ('maui path [GradleBuildSystem]pkg.dir') do set "GRADLE_
 echo Using gradle at %GRADLE_PATH%
 
 
-IF "%~1" == "" (
+if "%~1"==""
   echo Executing %GRADLE_PATH%/archipelagoGradleW build
   %GRADLE_PATH%/archipelagoGradleW build
-) else (
+else 
   echo Executing %GRADLE_PATH%/archipelagoGradleW %*
   %GRADLE_PATH%/archipelagoGradleW %*
-)
